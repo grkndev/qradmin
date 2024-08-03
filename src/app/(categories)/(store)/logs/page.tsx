e6be61edf6305e9f9862cbe9d2aea77b531d77b9
GweepCreative <gweepcreative@gmail.com>
2024-08-03 20:01:51 +0300

    Logs

--- src/app/(categories)/(store)/logs/page.tsx
+++ src/app/(categories)/(store)/logs/page.tsx
@@ -1,7 +1,93 @@
-import React from 'react'
+import React from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
+import { cn } from "@/lib/utils";
+import Icon from "@/components/Icon";
+
+type LogType = "info" | "error" | "warning" | "success";
+type Log = {
+  id: number;
+  message: string;
+  date: string;
+  icon: string;
+  type: LogType;
+};
+
+const logs: Log[] = [
+  {
+    id: 1,
+    message: "Hesabınız GRKNSOFT ile ilişkilendirildi.",
+    date: "2021-08-17T20:00:00Z",
+    type: "success",
+    icon: "Terminal",
+  },
+  {
+    id: 2,
+    message: "Hesabınız onaylandı.",
+    icon: "BadgeCheck",
+    date: "2021-08-17T20:00:00Z",
+    type: "success",
+  },
+  {
+    id: 3,
+    message: "Kurumsal hesaba geçiş yaptınız.",
+    date: "2021-08-17T20:00:00Z",
+    type: "warning",
+    icon: "Building2",
+  },
+  {
+    id: 4,
+    message: "Profil fotoğrafınızı güncellediniz.",
+    date: "2021-08-17T20:00:00Z",
+    type: "info",
+    icon: "UserRound",
+  },
+  {
+    id: 5,
+    message: "Hesabınızı oluşturdunuz.",
+    date: "2021-08-17T20:00:00Z",
+    type: "info",
+    icon: "UserRoundPlus",
+  },
+];
 
 export default function Logs() {
   return (
-    <div>Logs</div>
-  )
+    <div className="p-8 ml-6 flex flex-col gap-8 w-full">
+      <h1 className="text-3xl font-bold">Kayıtlar</h1>
+
+      <div className="w-full space-y-2">
+        {logs.map((log: Log) => (
+          <LogCard log={log} key={log.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+function LogCard({ log }: { log: Log }) {
+  return (
+    <Alert
+      className={cn(
+        "w-full",
+        log.type === "info" && "bg-blue-100",
+        log.type === "error" && "bg-red-100",
+        log.type === "warning" && "bg-yellow-100",
+        log.type === "success" && "bg-green-100"
+      )}
+    >
+      <Icon name={log.icon} className="h-4 w-4" />
+      <AlertTitle>{log.message}</AlertTitle>
+      <AlertDescription>
+        Tarih:{" "}
+        {new Date(log.date).toLocaleDateString("tr-TR", {
+          year: "numeric",
+          month: "2-digit",
+          day: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+        })}
+      </AlertDescription>
+    </Alert>
+  );
 }
